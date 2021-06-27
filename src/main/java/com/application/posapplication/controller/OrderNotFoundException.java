31fc357d63731c8d21e136d85df10e281e3d3b15
IceFairyFan <48240875+IceFairyFan@users.noreply.github.com>
2021-06-27 16:03:13 +0700

    Delete OrderNotFoundException.java

--- src/main/java/com/application/posapplication/controller/OrderNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.application.posapplication.controller;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "View not reached.")
-public class OrderNotFoundException extends RuntimeException{
-}
